d9487e0a035b80f763a236c77bbe46712bd0d4ec
tmwrp <tmwrpkv73@gmail.com>
2021-07-23 22:25:16 +0300

    LESSON-2-5: Complete task 5

---
+++ lesson 2/task5.js
@@ -0,0 +1,23 @@
+function findKayakAmount(N, D) {
+  const map = new Map();
+
+  for (const [firstIdx, personX] of N.entries()) {
+    map.set(firstIdx, personX);
+    for (const [secondIdx, personY] of N.entries()) {
+      if (personX !== personY) {
+        const twoPersonsWeight = personX + personY;
+        if (twoPersonsWeight <= D && map.get(firstIdx) < twoPersonsWeight) {
+          map.set(firstIdx, twoPersonsWeight);
+          if (map.has(secondIdx)) {
+            map.delete(secondIdx);
+          }
+        }
+      }
+    }
+  }
+
+  return console.log('The minimum amount of kayaks needed is:', map.size);
+}
+
+findKayakAmount([50, 74, 60, 82], 135); // 2
+findKayakAmount([50, 120, 74, 60, 100, 82], 135); // 4
